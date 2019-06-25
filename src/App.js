ff14e1bde0639016de5d6002544d662ec97fc178
Jimmy <jimmy@Jimmys-MacBook-Pro.local>
2019-06-25 15:45:54 +0800

    Modulize the components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import L from 'leaflet';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import { Card, Button, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap';
+import { getMessages, getLocation, sendMessage } from './API';
+import MessageCard from './MessageCard'; 
 import Joi from 'joi';
 
 import messageLocationURL from './simpleLocation.svg';
@@ -29,7 +30,6 @@ const schema = Joi.object().keys({
   message: Joi.string().min(1).max(500).required()
 });
 
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:5000/api/v1/messages' : 'production-url-here';
 
 class App extends Component {
 
@@ -50,51 +50,21 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch(API_URL)
-      .then(res => res.json())
+    getMessages()
       .then(messages => {
-        const haveSeenLocation = {};
-        messages = messages.reduce((all, message) => {
-          const key = `${message.latitude.toFixed(4)}${message.longitude.toFixed(4)}`;
-          if(haveSeenLocation[key]) {
-            haveSeenLocation[key].otherMessages = haveSeenLocation[key].otherMessages || [];
-            haveSeenLocation[key].otherMessages.push(message);
-          } else {
-            haveSeenLocation[key] = message; 
-            all.push(message);
-          }
-          return all;
-        }, []);
-
         this.setState({
           messages
         })
-      })
-
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.setState({
-        location: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        },
-        haveUsersLocation: true,
-        zoom: 13
       });
-    }, () => {
-      console.log('un oh...');
-      fetch('https://ipapi.co/json')
-        .then(res => res.json())
-        .then(location => {
-          this.setState({
-            location: {
-              lat: location.latitude,
-              lng: location.longitude
-            },
-            haveUsersLocation: false,
-            zoom: 1
-          });
-        })
-    });
+
+    getLocation()
+      .then(location => {
+        this.setState({
+          location,
+          haveUsersLocation: true,
+          zoom: 13
+        });
+      });
   }
 
   formIsValid = () => {
@@ -118,21 +88,15 @@ class App extends Component {
         sendingMessage: true
       });
 
+      const message = {
+        name: this.state.userMessage.name,
+        message: this.state.userMessage.message,
+        latitude: this.state.location.lat,
+        longitude: this.state.location.lng
+      };
 
-      fetch(API_URL, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: this.state.userMessage.name,
-          message: this.state.userMessage.message,
-          latitude: this.state.location.lat,
-          longitude: this.state.location.lng
-        })
-      }).then(res => res.json())
-        .then(message => {
-          console.log(message);
+      sendMessage(message)
+        .then((result) => {
           setTimeout(() => {
             this.setState({
               sendingMessage: false,
@@ -140,6 +104,7 @@ class App extends Component {
             });
           }, 4000);
         });
+      
     }
   }
 
@@ -178,41 +143,19 @@ class App extends Component {
               position={[message.latitude, message.longitude]}>
               <Popup>
                 <p><em>{message.name}:</em> {message.message}</p>
-                { message.otherMessages ? message.otherMessages.map(message => <p key="message._id"><em>{message.name}:</em> {message.message}</p>) : ''}
+                {message.otherMessages ? message.otherMessages.map(message => <p key="message._id"><em>{message.name}:</em> {message.message}</p>) : ''}
               </Popup>
             </Marker>
           ))}
         </Map>
-        <Card body className="message-form">
-          <CardTitle>Welcome to GuestMap!</CardTitle>
-          <CardText>Leave a message with your location!</CardText>
-          <CardText>Thanks for stopping by!</CardText>
-          {
-            !this.state.sendingMessage && !this.state.sentMessage && this.state.haveUsersLocation ?
-
-              <Form onSubmit={this.formSubmitted}>
-                <FormGroup>
-                  <Label for="message">Name</Label>
-                  <Input
-                    onChange={this.valueChanged}
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Enter your name" />
-                  <Label for="message">Message</Label>
-                  <Input
-                    onChange={this.valueChanged}
-                    type="textarea"
-                    name="message" id="message"
-                    placeholder="Enter your message" />
-                </FormGroup>
-                <Button color="info" disabled={!this.formIsValid()}>Send</Button>
-              </Form> :
-              this.state.sendingMessage || !this.state.haveUsersLocation ?
-                <img alt='Loading' src='https://media.giphy.com/media/51UxkLrSZNpj5a2BH6/giphy.gif'></img> :
-                <CardText>Thanks for submitting a message!</CardText>
-          }
-        </Card>
+        <MessageCard
+          sendingMessage={this.state.sendingMessage}
+          sentMessage={this.state.sentMessage}
+          haveUsersLocation={this.state.haveUsersLocation}
+          formSubmitted={this.formSubmitted}
+          valueChanged={this.valueChanged}
+          formIsValid={this.formIsValid}
+          />
       </div>
     )
   }
---
+++ src/MessageCard.js
@@ -0,0 +1,40 @@
+
+import React from 'react';
+
+import { Card, Button, CardTitle, CardText, Form, FormGroup, Label, Input } from 'reactstrap';
+
+
+export default (props) => {
+    return(
+        <Card body className="message-form">
+            <CardTitle>Welcome to GuestMap!</CardTitle>
+            <CardText>Leave a message with your location!</CardText>
+            <CardText>Thanks for stopping by!</CardText>
+            {
+                !props.sendingMessage && !props.sentMessage && props.haveUsersLocation ?
+
+                    <Form onSubmit={props.formSubmitted}>
+                        <FormGroup>
+                            <Label for="message">Name</Label>
+                            <Input
+                                onChange={props.valueChanged}
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Enter your name" />
+                            <Label for="message">Message</Label>
+                            <Input
+                                onChange={props.valueChanged}
+                                type="textarea"
+                                name="message" id="message"
+                                placeholder="Enter your message" />
+                        </FormGroup>
+                        <Button color="info" disabled={!props.formIsValid()}>Send</Button>
+                    </Form> :
+                    props.sendingMessage || !props.haveUsersLocation ?
+                        <img alt='Loading' src='https://media.giphy.com/media/51UxkLrSZNpj5a2BH6/giphy.gif'></img> :
+                        <CardText>Thanks for submitting a message!</CardText>
+            }
+        </Card>
+    )
+} 
